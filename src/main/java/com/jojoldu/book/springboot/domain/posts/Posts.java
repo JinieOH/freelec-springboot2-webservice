ba91857555b860d0b6906414f1758506ac48d7c6
JinieOH <hijinie20@gmail.com>
2020-06-21 17:48:02 +0900

    스터디용 CRUD 구성 추가

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -1,6 +1,5 @@
 package com.jojoldu.book.springboot.domain.posts;
 
-import javafx.geometry.Pos;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -35,6 +34,11 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 
 }
