880f8e8bcacf4f9ac9247958fdad7e96ad34a7ec
JinieOH <hijinie20@gmail.com>
2020-06-21 17:52:11 +0900

    Swagger 관련 소스 커밋

---
+++ src/main/java/com/jojoldu/book/springboot/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.jojoldu.book.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2 // Swagger2 버전을 활성화 하겠다는 어노테이션입니다
+public class SwaggerConfig {
+    private String version;
+    private String title;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("example.api.control"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+
+
+}
